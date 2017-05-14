f6a7ac52d8725db28f9dcc9230b8e581c0080991
Lawrence Moore <lawrence@sparkmeter.io>
2017-05-14 17:37:11 -0400

    Added Routes and respective links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { TodayComponent } from './today/today.component';
-import { ForecastComponent } from './forecast/forecast.component';
-import { RadarComponent } from './radar/radar.component';
-import { CitiesComponent } from './cities/cities.component';
+import {AppComponent} from './app.component';
+import {TodayComponent} from './today/today.component';
+import {ForecastComponent} from './forecast/forecast.component';
+import {RadarComponent} from './radar/radar.component';
+import {CitiesComponent} from './cities/cities.component';
+
+const appRoutes: Routes = [
+    {path: '', component: TodayComponent},
+    {path: 'today', component: TodayComponent},
+    {path: 'forecast', component: ForecastComponent},
+    {path: 'radar', component: RadarComponent},
+    {path: 'cities', component: CitiesComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodayComponent,
-    ForecastComponent,
-    RadarComponent,
-    CitiesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TodayComponent,
+        ForecastComponent,
+        RadarComponent,
+        CitiesComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
