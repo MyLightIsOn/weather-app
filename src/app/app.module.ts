814ab549aeb4e844363bf0ba7a4eb43f18a4be20
Lawrence Moore <lawrence@sparkmeter.io>
2017-05-14 17:10:41 -0400

    Added components for different pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TodayComponent } from './today/today.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { RadarComponent } from './radar/radar.component';
+import { CitiesComponent } from './cities/cities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodayComponent,
+    ForecastComponent,
+    RadarComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
